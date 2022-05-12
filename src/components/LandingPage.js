03df05fd280170ec9451bfe70f3df0091df34596
jbryanmcgrath <90098243+jbryanmcgrath@users.noreply.github.com>
2022-05-12 12:24:26 -0400

    Landing page started

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -1,8 +1,16 @@
 import React from 'react'
-import logo from '../images/logo.png'
+
 
 export const LandingPage = () => {
     return (
-        <div> <img src={logo} /></div>
+        <>
+            <div className='bg-deep-champagne bg-cover'>
+                <h1>Rabbit Valley Music Festival</h1>
+                <h2>
+                    October 9 & 10
+                </h2>
+                <button className='bg-medium-carmine  border-2 border-black text-black hover:text-white font-[Poppins] duration-500 px-6 py-2 mx-4 hover:bg-polished-pine rounded-full'>Buy Tickets</button>
+            </div>
+        </>
     )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,17 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'deep-champagne': '#EEC99Dff',
+        'tumbleweed': '#CBA083ff',
+        'polished-pine': '#479B90ff',
+        'hookers-green': '#4F7E75ff',
+        'medium-carmine': '#B8483Aff',
+        'orange-peel': '#F39A1Bff',
+        'black-olive': '#424035ff'
+      }
+    },
   },
   plugins: [],
 }
